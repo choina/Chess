b1e1f38bcd4555d5be82332808bf65b3a87dfbaf
choina <rafal.dziubak97@gmail.com>
2018-08-23 13:52:13 +0200

    Board included to project

---
+++ Chess/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //this.TopMost = true; // this will set it on top of everything, including the taskbar
+           // this.FormBorderStyle = FormBorderStyle.None; // remove the title bar
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
